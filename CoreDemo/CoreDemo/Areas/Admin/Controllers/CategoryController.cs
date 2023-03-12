e4f5b169602c02814c010df45d0af6320fa2f48c
Enes Cigdem <55703841+enesscigdem@users.noreply.github.com>
2023-03-12 04:39:31 +0300

    Update_12_03_23

---
+++ CoreDemo/CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class CategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index()
+        {
+            var values = cm.GetList();
+            return View(values);
+        }
+    }
+}
