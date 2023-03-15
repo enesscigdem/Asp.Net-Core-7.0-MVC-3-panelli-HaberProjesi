b0768b63ab030a82e6b1fdb693ae376892c6c112
enescigdem <55703841+enesscigdem@users.noreply.github.com>
2023-03-15 19:39:20 +0300

    15_03_23_Project_Updated_SQLAdminTableAdded_WidgetPageAdded_UsedApiForWeather

---
+++ CoreDemo/BusinessLayer/Concrete/AdminManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+	public class AdminManager : IAdminService
+    {
+		IAdminDal _Admindal;
+
+        public AdminManager(IAdminDal admindal)
+        {
+            _Admindal = admindal;
+        }
+
+        public List<Admin> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TAdd(Admin t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Admin t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Admin TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Admin t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ CoreDemo/CoreDemo/Areas/Admin/Controllers/WidgetController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class WidgetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
