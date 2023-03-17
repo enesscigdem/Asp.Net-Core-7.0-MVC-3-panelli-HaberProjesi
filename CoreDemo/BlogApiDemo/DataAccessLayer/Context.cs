ccfea1b6586432c28d6cbd1a1a8dcc085583db4f
enescigdem <55703841+enesscigdem@users.noreply.github.com>
2023-03-18 02:03:22 +0300

    Project_Updated_18_03_23_AddedBlogApiDemoLayer_ApiWith_Crud_Process

---
+++ CoreDemo/BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+	public class Context : DbContext
+	{
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // bağlantı stringimizi tanımlayacağız
+            optionsBuilder.UseSqlServer("server=localhost;uid=sa;password=reallyStrongPwd123;database=CoreBlogApiDb;TrustServerCertificate=true;");
+        }
+        public DbSet<Employee> Employees{ get; set; }
+    }
+}
+
---
+++ CoreDemo/BlogApiDemo/DataAccessLayer/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+	public class Employee
+	{
+		[Key]
+		public int ID { get; set; }
+		public string Name { get; set; }
+	}
+}
+
