8cdb6ee8846fe9980d7ae2d879246a72bba05030
enescigdem <55703841+enesscigdem@users.noreply.github.com>
2023-03-18 13:36:02 +0300

    18_03_23_Update

--- CoreDemo/CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreDemo/CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -18,7 +18,7 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
             ViewBag.v3 = c.Comments.Count();
 
             string api = "f3a7c1307ba0abbeeac09cc3f0b54bae";
-            string connection = "https://api.openweathermap.org/data/2.5/weather?q=izmir&mode=xml&lang=tr&units=metric&appid=" + api;
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + api;
             XDocument document = XDocument.Load(connection);
             ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             return View();
