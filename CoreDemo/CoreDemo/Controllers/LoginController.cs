e1787b9c0634bdbf9a829e18fd060551a3d8ef30
enescigdem <55703841+enesscigdem@users.noreply.github.com>
2023-03-24 18:45:45 +0300

    Updated

--- CoreDemo/CoreDemo/Controllers/LoginController.cs
+++ CoreDemo/CoreDemo/Controllers/LoginController.cs
@@ -3,41 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using CoreDemo.Models;
 using DataAccessLayer.Concrete;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
+    [AllowAnonymous]
     public class LoginController : Controller
     {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
         [HttpGet]
-        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        [AllowAnonymous]
-        public async Task<ActionResult> Index(Writer p)
+        public async Task<ActionResult> Index(UserSignInViewModel p)
         {
-            Context c = new Context();
-            var datavalue = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
-            if (datavalue!=null)
+            if (ModelState.IsValid)
             {
-                //HttpContext.Session.SetString("username", p.WriterMail);
-                var claims = new List<Claim>
+                var result = await _signInManager.PasswordSignInAsync(p.username, p.password, true, true); // buradaki 3. kısımdaki true değeri çerezlerde hatırlansın mı olayı. 4. kısım ise 5 den fazla yanlış deneme yaparsa kitlenecek.
+                if (result.Succeeded)
                 {
-                    new Claim(ClaimTypes.Name,p.WriterMail)
-                };
-                var useridentity = new ClaimsIdentity(claims, "a");
-                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
-                await HttpContext.SignInAsync(principal);
-                return RedirectToAction("Index", "Dashboard");
+                    return RedirectToAction("Index", "Dashboard");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Login");
+                }
             }
             return View();
         }
+
+
+        //[HttpGet]
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public async Task<ActionResult> Index(Writer p)
+        //{
+        //    Context c = new Context();
+        //    var datavalue = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
+        //    if (datavalue != null)
+        //    {
+        //        //HttpContext.Session.SetString("username", p.WriterMail);
+        //        var claims = new List<Claim>
+        //        {
+        //            new Claim(ClaimTypes.Name,p.WriterMail)
+        //        };
+        //        var useridentity = new ClaimsIdentity(claims, "a");
+        //        ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
+        //        await HttpContext.SignInAsync(principal);
+        //        return RedirectToAction("Index", "Dashboard");
+        //    }
+        //    return View();
+        //}
     }
 }
---
+++ CoreDemo/CoreDemo/Models/UserSignInViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace CoreDemo.Models
+{
+	public class UserSignInViewModel
+	{
+        [Required(ErrorMessage = "Lütfen kullanıcı adınızı giriniz")]
+        public string username { get; set; }
+		public string password { get; set; }
+	}
+}
+
