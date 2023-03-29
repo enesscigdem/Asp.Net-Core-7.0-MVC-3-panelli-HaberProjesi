02907e3a399b18e370e9f90fdd61afda1056748e
enescigdem <enesscigdem19@hotmail.com>
2023-03-29 03:48:38 +0300

    Update

---
+++ CoreDemo/CoreDemo/Areas/Admin/Controllers/AdminBlogController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminBlogController : Controller
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        public IActionResult Index()
+        {
+            var values = bm.GetBlogListWithCategory();
+            return View(values);
+        }
+    }
+}
